1c89bed84bef9e3d162ec49f0a02d4b4080987cd
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-01-24 15:07:54 -0500

    :construction: [ADD] Socket initial configurations

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const path = require('path')
 const app = express() // Initialize express
 require('dotenv').config() // Load .env file
 
+// Node server
+const server = require('http').createServer(app) // We connect our socket server with our application created with express
+const io = require('socket.io')(server)
+
+// Socket messages
+io.on('connection', (client) => {
+  console.log('Client connected')
+  client.on('disconnect', () => {
+    console.log('Client disconnected')
+  }) // Callback qhen some client disconnect from the server
+})
+
 // Public path
 const publicPath = path.resolve(__dirname, 'public')
 app.use(express.static(publicPath)) // We serve public folder
 
 // We start the server on the port 3000
-app.listen(process.env.PORT, (error) => {
+server.listen(process.env.PORT, (error) => {
   if (error) throw new Error(error)
   console.log(`Server is running on port ${process.env.PORT}`)
 })
