f85a6d753881e3013fa559c71d95b622d6e10d5d
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-01-24 15:21:45 -0500

    :construction: [ADD] Emit and send messages to the server

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const io = require('socket.io')(server)
 
 // Socket messages
 io.on('connection', (client) => {
-  console.log('Client connected')
+  // ON is for listening to the client
   client.on('disconnect', () => {
     console.log('Client disconnected')
   }) // Callback qhen some client disconnect from the server
+  client.on('message', (message) => {
+    console.log(message)
+    io.emit('message', { admin: 'New message' }) // Emit message to all clients connected
+  })
 })
 
 // Public path
