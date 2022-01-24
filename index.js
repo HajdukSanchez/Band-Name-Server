f1a0bc6ca36e383eb55cc10c4ee18cf17df8bc2c
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-01-24 14:35:48 -0500

    :construction: [ADD] Environment variables file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const path = require('path')
 const app = express() // Initialize express
+require('dotenv').config() // Load .env file
 
 // Public path
 const publicPath = path.resolve(__dirname, 'public')
 app.use(express.static(publicPath)) // We serve public folder
 
 // We start the server on the port 3000
-app.listen(3000, (error) => {
+app.listen(process.env.PORT, (error) => {
   if (error) throw new Error(error)
-  console.log(`Server is running on port 3000`)
+  console.log(`Server is running on port ${process.env.PORT}`)
 })
