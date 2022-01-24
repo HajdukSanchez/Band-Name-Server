0b90888cdbf40aecd18ddf61d5893460ed1e0eb2
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-01-24 13:05:51 -0500

    :construction: [ADD] Public File HTML

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express() // Initialize express
+
+// Public path
+const publicPath = path.resolve(__dirname, 'public')
+app.use(express.static(publicPath)) // We serve public folder
 
 // We start the server on the port 3000
 app.listen(3000, (error) => {
