e8c8848b8fcbcc36d3878c667593f592bac64728
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-01-25 16:43:41 -0500

    :construction: [ADD] Socket event from server

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,13 +1,13 @@
-const { io } = require('../index')
+const { io: server } = require('../index')
 
 // Socket messages
-io.on('connection', (client) => {
+server.on('connection', (client) => {
   // ON is for listening to the client
   client.on('disconnect', () => {
     console.log('Client disconnected')
   }) // Callback qhen some client disconnect from the server
-  client.on('message', (message) => {
-    console.log(message)
-    io.emit('message', { admin: 'New message' }) // Emit message to all clients connected
+
+  client.on('message', (payload) => {
+    client.broadcast.emit('message', payload) // Emit message to all clients expect one who send the message
   })
 })
